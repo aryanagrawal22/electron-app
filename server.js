2fde0dcdcbdab88ea1b5492a0aa0f144c7f4603a
aryanagrawal22 <aryanagrawal20023@gmail.com>
2022-12-29 00:38:42 +0530

    Electron: Setup

---
+++ server.js
@@ -0,0 +1,40 @@
+const {app, BrowserWindow, Menu} = require ('electron')
+
+let win
+
+app.on('ready', _ => {
+    win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    })
+    const template = [
+        {
+            label: 'Help',
+            submenu: [{ // adds submenu
+                    label: `About US`,
+                }, {
+                    type: 'separator' // horizontal line between submenu items
+                },{
+                    label: 'Quit',
+                    role: 'quit' // closes app when clicked
+
+                }]
+        },
+        {
+            label: 'Refresh', // Refreshes or reloads the page when clicked
+            role: 'reload'
+        },
+        {
+            label: 'ZoomIn', // zooms the page when clicked
+            role : 'ZoomIn'
+        },
+        {
+            label: 'ZoomOut', // zooms the page when clicked
+            role : 'ZoomOut'
+        }
+    ]
+    const menu = Menu.buildFromTemplate (template) // sets the menu
+    Menu.setApplicationMenu (menu)
+
+    win.loadURL('https://app.getswipe.in/')    // loads this URL
+})
